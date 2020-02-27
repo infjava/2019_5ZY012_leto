7a1bc2fe5e7ae7037521565a3a3ae11e70c23fa3
Jan Janech <jan.janech@kst.uniza.sk>
2020-02-27 15:29:43 +0100

    02b - nova mapa hracej plochy podla FRIcky

--- worldOfFRI/src/sk/uniza/fri/wof/prostredie/HraciaPlocha.java
+++ worldOfFRI/src/sk/uniza/fri/wof/prostredie/HraciaPlocha.java
@@ -14,24 +14,48 @@ public class HraciaPlocha {
     private final Miestnost pociatocnaMiestnost;
 
     public HraciaPlocha() {
-        // vytvorenie miestnosti
-        Miestnost terasa = new Miestnost("terasa - hlavny vstup na fakultu");
-        Miestnost aula = new Miestnost("aula");
-        Miestnost bufet = new Miestnost("bufet");
-        Miestnost labak = new Miestnost("pocitacove laboratorium");
-        Miestnost kancelaria = new Miestnost("kancelaria spravcu pocitacoveho laboratoria");
+        Miestnost bus = new Miestnost("autobus - ide sa domov");
+        Miestnost bufet = new Miestnost("bufet - tu ti daju bagetu a pizzu");
+        Miestnost parkovisko = new Miestnost("parkovisko - plne aut, bicyklov, kolobeziek, tiav a koni");
+        Miestnost rc = new Miestnost("rc - chodba v podzemi");
+        Miestnost ra006 = new Miestnost("ra006 - miestnost plna pilne programujucich studentov");
+        Miestnost ra = new Miestnost("ra - chodba s vytrinkami plnymi historickych artefaktov");
+        Miestnost vratnica = new Miestnost("vratnica - tu na nas ciha pani Vratnicka");
+        Miestnost rb = new Miestnost("rb - temna chodba");
+        Miestnost chillZone = new Miestnost("chillzona - mozte sa tu schladit v prijemnom prostredi huciacich pocitacov");
+        Miestnost ic = new Miestnost("ic - kniznica");
         
-        // inicializacia miestnosti = nastavenie vychodov
-        terasa.nastavVychod("vychod", aula);
-        terasa.nastavVychod("juh", labak);
-        terasa.nastavVychod("zapad", bufet);
-        aula.nastavVychod("zapad", terasa);
-        bufet.nastavVychod("vychod", terasa);
-        labak.nastavVychod("sever", terasa);
-        labak.nastavVychod("vychod", kancelaria);
-        kancelaria.nastavVychod("zapad", labak);
+        bus.nastavVychod("vychod", parkovisko);
+        
+        bufet.nastavVychod("sever", parkovisko);
+        bufet.nastavVychod("vychod", rc);
+        
+        parkovisko.nastavVychod("zapad", bus);
+        parkovisko.nastavVychod("hore", vratnica);
+        parkovisko.nastavVychod("sever", bufet);
+        
+        rc.nastavVychod("zapad", bufet);
+        rc.nastavVychod("hore", ra);
+        
+        ra006.nastavVychod("vychod", ra);
+        
+        ra.nastavVychod("zapad", ra006);
+        ra.nastavVychod("dole", rc);
+        ra.nastavVychod("juh", vratnica);
+        
+        vratnica.nastavVychod("juh", rb);
+        vratnica.nastavVychod("sever", ra);
+        vratnica.nastavVychod("vychod", ic);
+        vratnica.nastavVychod("dole", parkovisko);
+        
+        rb.nastavVychod("sever", vratnica);
+        rb.nastavVychod("juh", chillZone);
+        
+        chillZone.nastavVychod("sever", rb);
+        
+        ic.nastavVychod("zapad", vratnica);
 
-        this.pociatocnaMiestnost = terasa;
+        this.pociatocnaMiestnost = vratnica;
     }
 
     public Miestnost getPociatocnaMiestnost() {

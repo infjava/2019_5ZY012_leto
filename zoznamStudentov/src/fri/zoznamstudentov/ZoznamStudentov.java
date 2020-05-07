febcc472ecd85f1f52fce8bab24a3c651606c7a7
Jan Janech <jan.janech@kst.uniza.sk>
2020-05-07 15:48:55 +0200

    11b - systemova tema

--- zoznamStudentov/src/fri/zoznamstudentov/ZoznamStudentov.java
+++ zoznamStudentov/src/fri/zoznamstudentov/ZoznamStudentov.java
@@ -5,6 +5,8 @@
  */
 package fri.zoznamstudentov;
 
+import javax.swing.UIManager;
+
 /**
  *
  * @author janik
@@ -15,7 +17,11 @@ public class ZoznamStudentov {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+        }
+        
         HlavneOkno okno = new HlavneOkno();
         okno.setVisible(true);
     }

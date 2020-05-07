bfb0df35a80fe2acd89d780bd6c38e4033caaf8b
Jan Janech <jan.janech@kst.uniza.sk>
2020-05-07 14:25:04 +0200

    11b - projekt pre zoznam studentov uz s vytvorenym oknom

---
+++ zoznamStudentov/src/fri/zoznamstudentov/ZoznamStudentov.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fri.zoznamstudentov;
+
+/**
+ *
+ * @author janik
+ */
+public class ZoznamStudentov {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HlavneOkno okno = new HlavneOkno();
+        okno.setVisible(true);
+    }
+    
+}
